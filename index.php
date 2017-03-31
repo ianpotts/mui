be11aa93947f44f8e7747cd384e261fb2bbe8e9b
Tom Mairs <tmairs@aasland.com>
2017-03-31 12:49:40 -0600

    added body with Momo version

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 include ('header.php');
+
+echo "<h1>Welcome to MUI, a custom, open-source WebUI for Momentum 4.2.28-MTA-Only</h1><br>";
+$output = shell_exec'"version" |/opt/msys/ecelerity/bin/ec_console';
+echo "This is running on :<br>";
+echo "<PRE>$output</PRE>;
+
 include ('footer.php');
 ?>
