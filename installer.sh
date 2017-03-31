ffa4ca24ab0aa33be70bd1f0ab1bf3e7c68f1057
Tom Mairs <tmairs@aasland.com>
2017-03-31 12:12:15 -0600

    Updates to add Git

--- installer.sh
+++ installer.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+# Install Git
+sudo yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel -y
+sudo yum install gcc perl-ExtUtils-MakeMaker -y
+cd /usr/src
+wget https://www.kernel.org/pub/software/scm/git/git-2.9.3.tar.gz
+tar xzf git-2.9.3.tar.gz 
+cd git-2.9.3
+make prefix=/usr/local/git all
+make prefix=/usr/local/git install
+export PATH=$PATH:/usr/local/git/bin
+source /etc/bashrc
+
+# Install PHP
 sudo yum install php php-fpm -y
 sudo chkconfig --levels 235 php-fpm on
 
+# Updae Configs
 sudo sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php.ini
 sudo sed -i "s/user = apache/user = msysnginx/" /etc/php-fpm.d/www.conf
 sudo sed -i "s/group = apache/group = msysnginx/" /etc/php-fpm.d/www.conf
 
-
+# Build the web service
 sudo mkdir -p /var/www/momostatus
 
 sudo echo "
@@ -34,6 +48,7 @@ server {
 }
 " >/opt/msys/3rdParty/nginx/conf.d/webui.conf
 
+# Populate the WebUI
 sudo echo '<?php
 phpinfo();
 ?>' > /var/www/momostatus/info.php
@@ -45,6 +60,7 @@ echo "<html><body><p>PLACEHOLDER</p></body></html>";
 ?>
 '> /var/www/momostatus/index.php
  
+# Restart the services
 sudo service php-fpm restart
 sudo service msys-nginx restart
 
