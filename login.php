7e0e7f7f027ab7e83595beccef7e3de39181a108
Tom Mairs <tmairs@aasland.com>
2017-04-05 16:54:26 -0600

    Create login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+$uid = $_POST['username'];
+$key = $_POST['secret'];
+
+$_SESSION['ver'] = "false";
+
+echo "$uid <br>";
+echo "$key <br>";
+
+// Change this to something more secure at some point before publishing
+if (($uid == "admin") && ($key == "admin")){
+  $_SESSION['ver'] = "true";
+  echo "Allowing access for $uid <br>";
+}
+
+header('Location: index.php');
+
+exit;
+
+
+?>
