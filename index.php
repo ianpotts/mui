61d8484377fb92e53ae572e670472ca62f9b1ee8
Tom Mairs <tmairs@aasland.com>
2017-04-05 16:56:25 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 include ('header.php');
 
-echo "<h1>Welcome to MUI, a custom, open-source WebUI for Momentum 4.2.28-MTA-Only</h1><br>";
-$output = shell_exec'"version" |/opt/msys/ecelerity/bin/ec_console';
+echo "<h1>Welcome to MUI</h1> 
+      <h2>A custom, open-source WebUI for <br>
+          Momentum 4.2.28-MTA-Only</h2><br>";
+$output = shell_exec('echo "version" |/opt/msys/ecelerity/bin/ec_console');
+
 echo "This is running on :<br>";
-echo "<PRE>$output</PRE>;
+echo "<PRE>$output</PRE>";
 
 include ('footer.php');
 ?>
