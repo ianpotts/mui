c39b154cd7930399cf0ab9b979d1c15b6d2bb468
Tom Mairs <tmairs@aasland.com>
2017-04-18 22:17:43 -0600

    Rebuild to show a table

--- stats.php
+++ stats.php
@@ -1,22 +1,25 @@
 <?php
 include ('header.php');
 
-echo "<h1>Messaging Statistics</h1><br>";
+echo "<h1>Messaging Statistics</h1>";
 
-if ($a != 2){
-  echo "No data available";
+$output1 = shell_exec('cat /var/log/ecelerity/mainlog.ec |grep "@R@" |grep wc -l');
+$output2 = shell_exec('cat /var/log/ecelerity/mainlog.ec |grep "@D@" |grep wc -l');
+$output3 = shell_exec('cat /var/log/ecelerity/mainlog.ec |grep "@P@" |grep wc -l');
+if ($output1 < 1){$output1 = 0;}
+if ($output2 < 1){$output2 = 0;}
+if ($output3 < 1){$output3 = 0;}
+echo "<table border=1><tr><td>&nbsp;</td><td>Deliveries</td><td>Receptions</td><td>Bounces</td></tr>";
+echo "<tr><td><b>Today: </b></td><td>$output1</td><td>$output2</td><td>$output3</td></tr>";
 
-}
-echo "<br>";
-
-$output = shell_exec('cat /var/log/ecelerity/mainlog.ec |grep "@D@" |grep wc -l');
-if ($output < 1){$output = 0;}
-echo "<b>Deliveries today: </b>$output <br>";
-
-$output = shell_exec('bzcat /var/log/ecelerity/mainlog.ec.1.bz2 |grep "@D@" |grep wc -l');
-if ($output < 1){$output = 0;}
-echo "<b>Deliveries yesterday: </b>$output <br>";
-
+$output1 = shell_exec('bzcat /var/log/ecelerity/mainlog.ec.1.bz2 |grep "@R@" |grep wc -l');
+$output2 = shell_exec('bzcat /var/log/ecelerity/mainlog.ec.1.bz2 |grep "@D@" |grep wc -l');
+$output3 = shell_exec('bzcat /var/log/ecelerity/mainlog.ec.1.bz2 |grep "@P@" |grep wc -l');
+if ($output1 < 1){$output1 = 0;}
+if ($output2 < 1){$output2 = 0;}
+if ($output3 < 1){$output3 = 0;}
+echo "<tr><td><b>Yesterday: </b></td><td>$output1</td><td>$output2</td><td>$output3</td></tr>";
+echo "</table>";
 
 
 include ('footer.php');
