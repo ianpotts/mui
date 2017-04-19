493bfac3dff0aa5d9efaceadbd05e0d8d0181ea5
Tom Mairs <tmairs@aasland.com>
2017-04-18 21:54:32 -0600

    Add Linux version output

--- index.php
+++ index.php
@@ -5,9 +5,13 @@ echo "<h1>Welcome to MUI</h1>
       <h2>A custom, open-source WebUI for <br>
           Momentum 4.2.28-MTA-Only</h2><br>";
 $output = shell_exec('echo "version" |/opt/msys/ecelerity/bin/ec_console');
+$output1 = shell_exec('uname -a');
 
 echo "This is running on :<br>";
-echo "<PRE>$output</PRE>";
+echo "<PRE>
+$output
+$output1
+</PRE>";
 
 include ('footer.php');
 ?>
