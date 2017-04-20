d322481bb1d725ba8a9212e80a6ee33b314f7faa
Tom Mairs <tmairs@aasland.com>
2017-04-19 21:56:45 -0600

    Update summary.php

--- summary.php
+++ summary.php
@@ -1,8 +1,10 @@
 <?php
 include ('header.php');
 
+
 $output = shell_exec('echo "summary" |/opt/msys/ecelerity/bin/ec_console');
 echo "<PRE>$output</PRE>";
+
 
 $output = shell_exec('echo "active 0" |/opt/msys/ecelerity/bin/ec_console');
 echo "<b>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Active Queue size</b><br><PRE>$output</PRE>";
@@ -12,6 +14,8 @@ echo "<br>";
 $output = shell_exec('echo "delayed 0" |/opt/msys/ecelerity/bin/ec_console');
 echo "<b>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Delayed Queue size</b><br><PRE>$output</PRE>";
 
+
+
 $output = shell_exec('echo "adaptive suspensions" |/opt/msys/ecelerity/bin/ec_console');
 echo "<b>Adaptive Suspensions</b><br><PRE>$output</PRE>";
 
@@ -20,6 +24,7 @@ echo "<b>Adaptive List</b><br><PRE>$output</PRE>";
 
 $output = shell_exec('cat /var/log/ecelerity/adaptive.summary');
 echo "<b>Adaptive Summary</b><br><PRE>$output</PRE>";
+
 
 include ('footer.php');
 ?>
