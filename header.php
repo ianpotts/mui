865118c21bbad524f83ae669ba930b7feb16c936
Tom Mairs <tmairs@aasland.com>
2017-05-12 22:08:53 -0600

    update HTML header for wider acceptance

--- header.php
+++ header.php
@@ -4,8 +4,8 @@ $verified = $_SESSION['ver'];
 
 $mui_version = "0.3 detatched";
 
-echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
-<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">
+echo "<!DOCTYPE html> 
+<html>
 <head>
     <title>MUI</title>
     <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
